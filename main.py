430827e9905815cfdd5eab37e4915860d99b4195
TripsJ <jerome.trips@tutanota.com>
2026-07-26 10:00:26 +0200

    added exception on FileNotFound error and forced png as filetype

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import qrcode
 
-def create_qr(url:str,filename:str):
+
+def create_qr(url: str, filename: str):
     qrcode.make(url).save(filename)
 
-def main ():
+
+def main():
+    # taking user input
     url = input("give me a link: ")
     filename = input("give me a filename: ")
-    create_qr(url,filename)
-    
-if __name__=="__main__" :
+    # removing anything after a . so therearent double fileextensions
+
+    sep = "."
+    stripped_filename = filename.split(sep, 1)[0]
+    try:
+        create_qr(url, f"{stripped_filename}.png")
+    except FileNotFoundError:
+        print("The specified file could not be created. Did you set a filename?")
+        main()
+
+
+if __name__ == "__main__":
     main()
-    

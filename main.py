0d36217add9ea41ae886b38ef5777b085449d692
TripsJ <jerome.trips@tutanota.com>
2026-07-26 12:08:48 +0200

    removed test image from git, refactored code to prepare for UI Integration.

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-import qrcode
 import sys
 
+import qrcode
+import ttkbootstrap as ttk
 
 def create_qr(url: str, filename: str):
     qrcode.make(url).save(filename)
 
-
-def main():
+def remove_extension(name:str)->str:
+    sep = "."
+    stripped_filename = name.split(sep, 1)[0]
+    if len(stripped_filename) < 1:
+        raise FileNotFoundError
+    else:
+        return stripped_filename
+    
+def text_mode():
     # taking user input
     url = input("give me a link: ")
     filename = input("give me a filename: ")
     # removing anything after a . so therearent double fileextensions
     try:
-        sep = "."
-        stripped_filename = filename.split(sep, 1)[0]
-        if len(stripped_filename) <1:
-            raise FileNotFoundError
+        str_name=remove_extension(filename)
     except FileNotFoundError:
         print("no filename specified, Abandonning")
         sys.exit()
     try:
-        create_qr(url, f"{stripped_filename}.png")
+        create_qr(url, f"{str_name}.png")
     except FileNotFoundError:
         print("The specified file could not be created.")
-        
-    
+
+
+
+def main():
+    text_mode()
+
 
 if __name__ == "__main__":
     main()

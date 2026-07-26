01d67f040458ab3f6056fb51459635666c4870e6
TripsJ <jerome.trips@tutanota.com>
2026-07-26 10:15:12 +0200

    now exiting on empty filenames

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import qrcode
+import sys
 
 
 def create_qr(url: str, filename: str):
@@ -10,15 +11,20 @@ def main():
     url = input("give me a link: ")
     filename = input("give me a filename: ")
     # removing anything after a . so therearent double fileextensions
-
-    sep = "."
-    stripped_filename = filename.split(sep, 1)[0]
+    try:
+        sep = "."
+        stripped_filename = filename.split(sep, 1)[0]
+        if len(stripped_filename) <1:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        print("no filename specified, Abandonning")
+        sys.exit()
     try:
         create_qr(url, f"{stripped_filename}.png")
     except FileNotFoundError:
-        print("The specified file could not be created. Did you set a filename?")
-        main()
-
+        print("The specified file could not be created.")
+        
+    
 
 if __name__ == "__main__":
     main()

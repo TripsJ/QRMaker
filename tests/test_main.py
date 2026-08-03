b58d50c163ba82cd67d3c7ee8f0f9ba311c1f40e
TripsJ <jerome.trips@tutanota.com>
2026-08-03 18:34:32 +0200

    tests for remove extensions

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+"""Tests for QRMakers main.py."""
+
+import pytest
+
+from main import remove_extension
+
+
+def test_remove_extension():
+    """Testing if extension removal works for different extensions."""
+    assert remove_extension("photo.png") == "photo"
+    assert remove_extension("photo.tar.gz") == "photo"
+    assert remove_extension("photo.bmp") == "photo"
+
+    def test_raises_Value_Error_on_empty_name():
+        """Testing if ValueError gets raised when expected."""
+        with pytest.raises(ValueError):
+            remove_extension("")
+            remove_extension(".bmp")

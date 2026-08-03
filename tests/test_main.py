6c48bf83e7ecf5e2dc32457805be322419873644
TripsJ <jerome.trips@tutanota.com>
2026-08-03 19:08:32 +0200

    added first test for create_qr

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,10 @@
 """Tests for QRMakers main.py."""
 
+from pathlib import Path
+
 import pytest
 
-from main import remove_extension
+from main import create_qr, remove_extension
 
 
 def test_remove_extension() -> None:
@@ -22,3 +24,20 @@ def test_raises_value_error_on_empty_name() -> None:
     ):  # the context manager gets exited as son as the first ValueError is encountered,
         # so you need one per check
         remove_extension(".bmp")
+
+
+def test_file_creation_for_create_qr(
+    tmp_path: Path,
+) -> None:  # tmp_path:Path is used to create a temporary
+    # directory that gets cleaned up after testing
+    """Testing if create qr actually creates filesystem.
+
+    Arguments:
+        tmp_path: Fixture to create a temporary directory.
+
+    """
+    target = tmp_path / "test.png"
+    create_qr(
+        "https://example.com", str(target)
+    )  # str makes sure the temporary path gets converted into a string
+    assert target.is_file()  # check if test.png exists
